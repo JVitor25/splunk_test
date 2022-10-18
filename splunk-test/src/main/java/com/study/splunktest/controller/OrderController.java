e45d14f0955c1849bd57c06e1ea23fc10656b9f1
Joao Vitor Lima <joaovitorsocial25@gmail.com>
2022-10-18 10:26:49 -0300

    Aplicação finalizad (enviando Logs para o Splunk)

--- splunk-test/src/main/java/com/study/splunktest/controller/OrderController.java
+++ splunk-test/src/main/java/com/study/splunktest/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.study.splunktest.controller;
 
 import com.study.splunktest.model.Order;
 import com.study.splunktest.service.OrderService;
+import com.study.splunktest.utility.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,24 +14,31 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
 
+    Logger logger = LogManager.getLogger(OrderService.class);
+
     @Autowired
     private OrderService orderService;
 
     @GetMapping
-    public List<Order> getOrder(){
+    public List<Order> getOrders(){
         List<Order> orders = orderService.getOrders();
+        logger.info("OrderController:placeOrder response from service {}", Mapper.mapToJsonString(orders));
         return orders;
     }
 
     @GetMapping("/{id}")
     public Order getOrder(@PathVariable int id){
+        logger.info("OrderController:getOrder fetch order by id '{}'", id);
         Order order = orderService.getOrder(id);
+        logger.info("OrderController:placeOrder fetch order response {}", Mapper.mapToJsonString(order));
         return order;
     }
 
     @PostMapping
     public Order placeOrder(@RequestBody Order order){
+        logger.info("OrderController:placeOrder persist order request {}", Mapper.mapToJsonString(order));
         Order addOrder = orderService.addOrder(order);
+        logger.info("OrderController:placeOrder response from service {}", Mapper.mapToJsonString(addOrder));
         return addOrder;
     }
 }
--- splunk-test/src/main/java/com/study/splunktest/service/OrderService.java
+++ splunk-test/src/main/java/com/study/splunktest/service/OrderService.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import com.study.splunktest.utility.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
@@ -13,25 +16,35 @@ import com.study.splunktest.model.Order;
 @Service
 public class OrderService {
 
+	Logger logger = LogManager.getLogger(OrderService.class);
+
 	private List<Order> orderList = new ArrayList<>();
 
 	public List<Order> getOrders(){
+		logger.info("OrderService:getOrders execution started...");
 		List<Order> list = null;
 		list = orderList;
+		logger.info("OrderService:addOrder response {}", Mapper.mapToJsonString(orderList));
+		logger.info("OrderService:getOrders execution ended...");
 		return list;
 	}
 
 	public Order getOrder(int id){
+		logger.info("OrderService:getOrder execution started...");
 		Order order = orderList.stream()
 				.filter(ord -> ord.getId() == id)
 				.findAny().orElseThrow(() -> new RuntimeException("Order not found with id : " + id));
+		logger.info("OrderService:getOrder execution ended...");
 		return order;
 	}
 
 	public Order addOrder(Order order) {
+		logger.info("OrderService:addOrder execution started...");
+		logger.info("OrderService:addOrder request payload {}", Mapper.mapToJsonString(order));
 		order.setOrderPlacedDate(new Date());
 		order.setTransactionId(UUID.randomUUID().toString());
 		orderList.add(order);
+		logger.info("OrderService:addOrder execution ended...");
 		return order;
 	}
 }
